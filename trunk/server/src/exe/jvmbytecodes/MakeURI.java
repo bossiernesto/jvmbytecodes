3ffd5cb4a62dbcaed21c004979240fb368ac7bfe
clemew37 <clemew37@5609e951-565a-9fe9-36e2-8a8072d6cfee>
2010-06-30 21:46:02 +0000

    does recursion, and call stack for methods -Will C.

--- trunk/server/src/exe/jvmbytecodes/MakeURI.java
+++ trunk/server/src/exe/jvmbytecodes/MakeURI.java
@@ -22,12 +22,14 @@ public class MakeURI {
 	static String make_uri(int[] lines, int[] colors, Frame_ f) {
 		String sSize = f.stackSize == -1 ? "null" : String.valueOf(f.stackSize);
 		String hSize = Driver.heapSize == -1 ? "null" : String.valueOf(Driver.heapSize);
-		String current_method_name = Driver.classes[Driver.currentClass].methods.get(Driver.currentMethod).name;
+		
+		//print the call stack in the display
+		String methodCallStack = printCallStack();
 
 		HashMap<String, String> map = new HashMap<String, String>();
 		map.put("stack_size", sSize);
 		map.put("heap_size", hSize);
-		map.put("method_name", current_method_name);
+		map.put("method_call_stack", methodCallStack);
 		String uri = null;
 
 		try {
@@ -41,17 +43,35 @@ public class MakeURI {
 	}
 
 	/*
+	 * Returns a string that contains the method call stack.
+	 */
+	static String printCallStack() {
+		String returnStr =" ";
+		Frame_ tempFrame;
+
+		if (!Driver._runTimeStack.empty())
+			returnStr += ((Frame_) Driver._runTimeStack.get(0)).methodName;
+
+		for (int i=1; i<Driver._runTimeStack.size(); i++)
+			returnStr += " -> "+((Frame_) Driver._runTimeStack.get(i)).methodName;
+			
+		return returnStr;
+	}
+
+	/*
 	 * This method displays the java class
 	 */
 	static String doc_uri(int line, Frame_ f) {
 		String sSize = f.stackSize == -1 ? "null" : String.valueOf(f.stackSize);
 		String hSize = Driver.heapSize == -1 ? "null" : String.valueOf(Driver.heapSize);
-		String current_method_name = Driver.classes[Driver.currentClass].methods.get(Driver.currentMethod).name;
+		
+		//print the call stack in the display
+		String methodCallStack = printCallStack();
 
 		HashMap<String, String> map = new HashMap<String, String>();
 		map.put("stack_size", sSize);
 		map.put("heap_size", hSize);
-		map.put("method_name", current_method_name);
+		map.put("method_call_stack", methodCallStack);
 		String uri = null;
 
 		// convert the byte code line number to the java line number
